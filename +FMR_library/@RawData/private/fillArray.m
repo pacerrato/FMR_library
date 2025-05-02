163dd474026b5ec51f1fd30a5fe1dfd4001bdcec
Pablo Cerrato Serrano <51543079+Pablete6@users.noreply.github.com>
2025-05-02 15:31:04 +0200

    [FIX] plot2d leaves blank gaps when data is missing

--- +FMR_library/@RawData/private/fillArray.m
+++ +FMR_library/@RawData/private/fillArray.m
@@ -1,4 +1,4 @@
-function F = fillArray(obj, A)
+function [F, x] = fillArray(obj, A)
 %FILLARRAY - Fill missing values in array with NaN
 %   This FMR-Library function fills the missing values in the array
 %   by filling them with NaN. This function is made to try and fix
@@ -6,7 +6,7 @@ function F = fillArray(obj, A)
 %   results.
 %
 %   Syntax
-%     F = FILLARRAY(obj, A)
+%     [F,x] = FILLARRAY(obj, A)
 %
 %   Input Arguments
 %     obj - Data object
@@ -17,14 +17,13 @@ function F = fillArray(obj, A)
 %   Output Arguments
 %     F - Filled array
 %       matrix
+%     x - X values
+%       1-by-N array
 arguments
     obj (1,1) FMR_library.RawData
     A {mustBeNumeric, mustBeMatrix}
 end
     warning("Data is incomplete or badly formatted. Filling missing data with NaN. Unexpected results may happen.");
-
-    % Initialize output
-    F = NaN(obj.nFrequencies, obj.nFields);
 
     % Get frequency and gain indices
     freqIdx = obj.frequencyIdx;
@@ -37,6 +36,17 @@ end
     currentField = A(1,fieldIdx);
     freqValues = unique(obj.getDataColumn("Frequency"));
 
+    % Calculate the number of gaps and number of fields
+    fieldValues = unique(A(:,fieldIdx));
+    gaps = diff(fieldValues);
+    deltaField = median(gaps);
+    nFields = obj.nFields + sum(gaps > 1.5 * deltaField, 'all');
+
+    % Initialize output
+    F = NaN(obj.nFrequencies, nFields);
+    x = zeros(nFields, 1);
+    x(1) = A(1, fieldIdx);
+
     % Fill the matrix with the gain values
     for i = 1:size(A,1)
         % If a new frequency value is reached, reset column, 
@@ -44,7 +54,13 @@ end
         if (currentField ~= A(i, fieldIdx))
             currentRow = 1;
             currentColumn = currentColumn + 1;
+            % Leave a NaN column if there's a gap
+            if (abs(A(i,fieldIdx) - currentField) > 1.5 * deltaField)
+                x(currentColumn) = currentField + deltaField;
+                currentColumn = currentColumn + 1;
+            end
             currentField = A(i, fieldIdx);
+            x(currentColumn) = currentField;
         end
         while (freqValues(currentRow) ~= A(i, freqIdx))
             currentRow = currentRow + 1;
