2ad85f433bf37d7c71d814198ff88437fce9a589
Pablo Cerrato Serrano <51543079+Pablete6@users.noreply.github.com>
2025-05-07 21:57:54 +0200

    [ADD] background polynomial degree pop up selector

--- +FMR_library/@RawData/private/makeResonanceFit.m
+++ +FMR_library/@RawData/private/makeResonanceFit.m
@@ -32,7 +32,7 @@ end
     initialParams = getInitialParams(fitData, figObject);
 
     % Make fit
-    [fitresult, gof] = fitVoigt(fitData, initialParams, figObject.quadraticBackground);
+    [fitresult, gof] = fitVoigt(fitData, initialParams, figObject.backgroundDeg);
     
     % Save fit parameters
     figObject.saveFitParams(fitresult, gof);
